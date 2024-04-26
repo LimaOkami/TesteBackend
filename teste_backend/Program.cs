439d22dae0f4339a55c30f5b27a0f60922ca9bcc
LimaOkami <139416710+LimaOkami@users.noreply.github.com>
2024-04-25 23:57:10 -0300

    Update Program.cs

--- teste_backend/Program.cs
+++ teste_backend/Program.cs
@@ -2,16 +2,12 @@ using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,14 +26,14 @@ using (MySqlConnection connection = new MySqlConnection(connectionString))
 {
     connection.Open();
 
-    // Verificar se a tabela est· vazia
+    // Verificar se a tabela est√° vazia
     string countQuery = "SELECT COUNT(*) FROM cliente";
     MySqlCommand countCommand = new MySqlCommand(countQuery, connection);
     int count = Convert.ToInt32(countCommand.ExecuteScalar());
 
     if (count == 0)
     {
-        // Se a tabela estiver vazia, inserir dados fictÌcios
+        // Se a tabela estiver vazia, inserir dados fict√≠cios
         string insertQuery = "INSERT INTO cliente (NomeCompleto, DataNascimento, TipoPessoa, CPF_CNPJ, Email, Endereco) " +
                              "VALUES (@NomeCompleto, @DataNascimento, @TipoPessoa, @CPFCNPJ, @Email, @Endereco)";
 
@@ -45,10 +41,10 @@ using (MySqlConnection connection = new MySqlConnection(connectionString))
                             {
                                 new { NomeCompleto = "Fulano de Tal", DataNascimento = new DateTime(1990, 1, 1), TipoPessoa = "F", CPFCNPJ = "123.456.789-00", Email = "fulano@example.com", Endereco = "Rua Exemplo, 123" },
                                 new { NomeCompleto = "Beltrano da Silva", DataNascimento = new DateTime(1985, 5, 15), TipoPessoa = "J", CPFCNPJ = "12.345.678/0001-99", Email = "beltrano@example.com", Endereco = "Avenida Teste, 456" },
-                                new { NomeCompleto = "Ciclano Oliveira", DataNascimento = new DateTime(1995, 10, 30), TipoPessoa = "F", CPFCNPJ = "987.654.321-00", Email = "ciclano@example.com", Endereco = "PraÁa do Exemplo, 789" }
+                                new { NomeCompleto = "Ciclano Oliveira", DataNascimento = new DateTime(1995, 10, 30), TipoPessoa = "F", CPFCNPJ = "987.654.321-00", Email = "ciclano@example.com", Endereco = "Pra√ßa do Exemplo, 789" }
                             };
 
-        // Inserir dados fictÌcios
+        // Inserir dados fict√≠cios
         foreach (var dados in dadosFicticios)
         {
             using (MySqlCommand command = new MySqlCommand(insertQuery, connection))
